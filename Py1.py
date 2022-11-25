e343b11042625a4d36d9c113fcbc1933cf1222c9
Guy <guykibby@gmail.com>
2022-11-26 10:02:49 +1300

    Simple scatter plot and yhat

---
+++ Py1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 25 18:45:17 2022
+
+@author: guykibby
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load Data from CSV to arrays
+X = []
+Y = []
+for line in open('data_1d.csv'):
+    x, y = line.split(',')
+    X.append(float(x))
+    Y.append(float(y))
+
+# convert to numpy arrays
+X = np.array(X)    
+Y = np.array(Y)
+
+# plot array data onto graph
+plt.scatter(X,Y)
+plt.show()
+
+# calculate the line of best fit
+denom = X.dot(X) - X.mean() * X.sum()
+a = (Y.dot(X) - Y.mean() * X.sum()) / denom
+b = ( Y.mean() * X.dot(X) - X.mean() * Y.dot(X))  / denom
+
+# calculate Y of best fit line
+yhat = a*X + b
+
+# plot the data w line of best fit
+plt.scatter(X,Y)
+plt.plot(X,yhat)
+plt.show()
